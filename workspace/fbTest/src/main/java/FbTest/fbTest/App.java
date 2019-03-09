ac72afa0cf7b244c3e14949aaf80b76e7db72773
TTL <TTL@192.168.1.106>
2019-03-09 18:36:05 +0530

    first commit

---
+++ workspace/fbTest/src/main/java/FbTest/fbTest/App.java
@@ -0,0 +1,13 @@
+package FbTest.fbTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
